aaf0afde979532f70cb5c84513b76896ef401ede
Andrey Solodov <legandr.86@gmail.com>
2016-10-28 19:51:30 +0400

    router simple app

---
+++ router-samples/app/main.ts
@@ -0,0 +1,18 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import HomeComponent from './home.component';
+import ProductDetailComponent from './product-detail.component';
+import AppComponent from './app.component';
+import { routing } from './app.routing';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+@NgModule({
+	imports: [ BrowserModule, routing ],
+	declarations: [ AppComponent, HomeComponent, ProductDetailComponent ],
+	providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
+	bootstrap: [ AppComponent ]
+})
+class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ router-samples/app/product-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'product',
+	template: '<h1 class="product">Product Details Component</h1>',
+	styles: ['.home {background:  cyan}']
+})
+export class ProductDetailComponent {}
