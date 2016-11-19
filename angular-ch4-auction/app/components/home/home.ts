461ba056255568093775a7976eca5c78233ad467
Andrey Solodov <andrey.solodov@aurea.com>
2016-11-19 18:57:43 +0400

    auction app ch4 added

---
+++ angular-ch4-auction/app/components/home/home.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+import {ProductService} from '../../services/product-service';
+
+@Component({
+    selector: 'auction-home-page',
+    styleUrls: ['/home.css'],
+    template: `
+        <div class="row carousel-holder">
+            <div class="col-md-12">
+                <auction-carousel></auction-carousel>
+            </div>
+        </div>
+        <div class="row">
+            <div *ngFor="let product of products" class="col-sm-4 col-lg-4 col-md-4">
+                <auction-product-item [product]="product"></auction-product-item>
+            </div>
+        </div>
+    `
+})
+export default class HomeComponent{
+    products: Array<Product> = [];
+    constructor(private productService: ProductService){
+        this.products = this.productService.getProducts();
+    }
+}
---
+++ angular-ch4-auction/app/components/product-item/product-item.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import StarsComponent from 'app/components/stars/stars';
+import { Product } from 'app/services/product-service';
+
+@Component({
+	selector: 'auction-product-item',
+	styleUrls: ['app/components/product-item/product-item.css'],
+	templateUrl: 'app/components/product-item/product-item.html'
+})
+export default class ProductItemComponent{
+	@Input() product: Product;
+}
