b2f3148519f8dba3751c1b788dfa01c452c99381
Andrey Solodov <andrey.solodov@aurea.com>
2016-11-21 22:47:58 +0400

    two-way binding added

--- bindings/systemjs.config.js
+++ bindings/systemjs.config.js
@@ -16,11 +16,12 @@ System.config({
 		'@angular/*': {'format': 'cjs'}
 	},
 	packages: {
-		'app' : {main: 'template-binding', defaultExtension: 'ts'},
+		'app' : {main: 'two-way-binding', defaultExtension: 'ts'},
 		'rxjs' : {main: 'Rx'},
 		'@angular/core' : {main: 'core.umd.min.js'},
 		'@angular/common' : {main: 'common.umd.min.js'},
 		'@angular/compiler' : {main: 'compiler.umd.min.js'},
+		'@angular/forms' : {main: 'forms.umd.min.js'},
 		'@angular/router' : {main: 'router.umd.min.js'},
 		'@angular/platform-browser' : {main: 'platform-browser.umd.min.js'},
 		'@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
