335762396ce1452ce4d7690bab520950e1a49503
Andrey Solodov <legandr.86@gmail.com>
2016-11-24 13:52:42 +0400

    observables temp app added

--- bindings/systemjs.config.js
+++ bindings/systemjs.config.js
@@ -16,13 +16,14 @@ System.config({
 		'@angular/*': {'format': 'cjs'}
 	},
 	packages: {
-		'app' : {main: 'two-way-binding', defaultExtension: 'ts'},
+		'app' : {main: 'observable-events-http', defaultExtension: 'ts'},
 		'rxjs' : {main: 'Rx'},
 		'@angular/core' : {main: 'core.umd.min.js'},
 		'@angular/common' : {main: 'common.umd.min.js'},
 		'@angular/compiler' : {main: 'compiler.umd.min.js'},
 		'@angular/forms' : {main: 'forms.umd.min.js'},
 		'@angular/router' : {main: 'router.umd.min.js'},
+		'@angular/http' : {main: 'http.umd.min.js'},
 		'@angular/platform-browser' : {main: 'platform-browser.umd.min.js'},
 		'@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
 	}
